b7ec606999426655bcc91c0c8d29a41bd211fc82
jlambdin <jlambdin@rams.colostate.edu>
2020-10-26 23:10:11 -0600

    10.1 10/27 database setup and /movies

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const db = require('./dbConnectExec.js')
 const app = express();
 
 app.get("/hi",(req, res)=>{
     res.send("Hello world")
 })
 
+app.get("/movies", (req, res)=>{
+    //Get Data from database
+    db.executeQuery(`SELECT * FROM movie LEFT JOIN genre ON genre.GenrePK = movie.GenreFK`)
+    .then((result)=>{
+        res.status(200).send(result)
+    })
+    .catch((err)=>{
+        console.log(err);
+        res.status(500).send()
+    })
+})
 app.listen(5000,()=>{console.log("App is running on port 5000")})
